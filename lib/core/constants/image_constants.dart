7cc681955502f46c88621d13a3d5378a575c92f0
Mustafa TÜRKMEN <iletisim@mustafatrkmn.com>
2021-08-16 21:59:46 +0300

    Core file added

---
+++ lib/core/constants/image_constants.dart
@@ -0,0 +1,8 @@
+class ImageConstants {
+  static ImageConstants instance = ImageConstants._();
+
+  ImageConstants._();
+  static const String _path = 'assets/images';
+  final String background = '$_path/bg.jpeg';
+  final String profile = '$_path/profile.png';
+}
